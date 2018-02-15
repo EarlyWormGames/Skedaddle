88f1b704ac157cd860acee67da8fac363c5fddd9
wickedarc <bernard.larkin@hotmail.com>
2018-02-15 15:03:02 +1100

    Submitting shit before push
    Fading transition and NV transition

--- Assets/Scripts/Aesthetic/Animal Aesthetics/LorisNightVision.cs
+++ Assets/Scripts/Aesthetic/Animal Aesthetics/LorisNightVision.cs
@@ -6,14 +6,27 @@ using System;
 
 public class LorisNightVision : MonoBehaviour
 {
+    public enum EStatus
+    {
+        eFADEIN,
+        eFADEOUT,
+        eIDLE,
+    };
+
     public bool NightVisionOn = false;
     public GameObject NightVisionObject;
     public float TranssionSpeed = 1.0f;
 
+    private bool PreviousNVStatus;
     private RawImage NV_Image;
     private Material Temp_NV_Material;
 
+    private EStatus m_eCurrentStatus;
+    private EStatus m_ePreviousStatus;
+    private bool m_bStatusJustChanged = false;
 
+    private GameManager GM;
+    private Fading m_Fade;
 
     private float NV_ActiveSensitivity = 0;
     private float NV_SensitivityMin = 2;
@@ -21,28 +34,92 @@ public class LorisNightVision : MonoBehaviour
 
     void Start()
     {
+        m_eCurrentStatus = EStatus.eIDLE;
+        m_ePreviousStatus = m_eCurrentStatus;
+
+        PreviousNVStatus = NightVisionOn;
+
+        m_Fade = GameManager.Instance.GetComponent<Fading>();
+
         NV_Image = NightVisionObject.GetComponent<RawImage>();
         Temp_NV_Material = Instantiate(NV_Image.material);
         NV_Image.material = Temp_NV_Material;
+
+        NightVisionObject.SetActive(false);
 
         if (NightVisionOn)
             NV_ActiveSensitivity = NV_SensitivityMin;
         else
             NV_ActiveSensitivity = NV_SensitivityMax;
-
+       
 
     }
 
+    private void OnDestroy()
+    {
+        DestroyImmediate(Temp_NV_Material);
+    }
     void Update()
     {
-        if (NightVisionOn)
+        
+
+        if (m_eCurrentStatus == EStatus.eIDLE)
         {
-            NV_ActiveSensitivity = NV_ActiveSensitivity - Time.deltaTime * TranssionSpeed;
+            if (PreviousNVStatus != NightVisionOn)
+            {
+                PreviousNVStatus = NightVisionOn;
+                if (NightVisionOn)
+                {
+                    
+
+                    if (NightVisionObject.activeSelf == false)
+                        NightVisionObject.SetActive(true);
+                    m_eCurrentStatus = EStatus.eFADEIN;
+                }
+                else
+                {
+                    m_eCurrentStatus = EStatus.eFADEOUT;
+
+                }
+            }
         }
-        else
+       
+        switch (m_eCurrentStatus)
         {
-            NV_ActiveSensitivity = NV_ActiveSensitivity + Time.deltaTime * TranssionSpeed;
+            case EStatus.eIDLE:
+                if (m_ePreviousStatus == EStatus.eFADEOUT)
+                {
+                    NightVisionObject.SetActive(false);
+                }
+
+                break;
+
+            case EStatus.eFADEIN:
+                
+                NV_ActiveSensitivity = NV_ActiveSensitivity - Time.deltaTime * TranssionSpeed;
+
+                if (NV_ActiveSensitivity <= NV_SensitivityMin)
+                {
+                    m_ePreviousStatus = m_eCurrentStatus;
+                    m_eCurrentStatus = EStatus.eIDLE;
+                }
+
+                break;
+            case EStatus.eFADEOUT:
+                NV_ActiveSensitivity = NV_ActiveSensitivity + Time.deltaTime * TranssionSpeed;
+
+                if (NV_ActiveSensitivity >= NV_SensitivityMax)
+                {
+                    m_ePreviousStatus = m_eCurrentStatus;
+                    m_eCurrentStatus = EStatus.eIDLE;
+                }
+                break;
+
+            default:
+                Debug.LogError("Fade Status is broken, Who know's why?");
+                break;
         }
+        
 
         NV_ActiveSensitivity = Mathf.Clamp(NV_ActiveSensitivity, NV_SensitivityMin, NV_SensitivityMax);
 
