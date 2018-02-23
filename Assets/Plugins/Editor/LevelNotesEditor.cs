d8089fb343e417939ba0291636aadfddb09e72f4
Joshua Jones <joshuajones2010@gmail.com>
2018-02-24 10:23:43 +1100

    Notes now have T H I C C curves :fire:

--- Assets/Plugins/Editor/LevelNotesEditor.cs
+++ Assets/Plugins/Editor/LevelNotesEditor.cs
@@ -11,7 +11,8 @@ public class LevelNotesEditor
     private static LevelNotes.TextPoint currentPoint;
     private static LevelNotes currentFlow;
 
-    private static LevelNotes.TextPoint editPoint, deletePoint;
+    private static LevelNotes.TextPoint editPoint, deletePoint, joinNode;
+    private static List<LevelNotes.TextPoint> otherNotes;
 
     static LevelNotesEditor()
     {
@@ -39,10 +40,45 @@ public class LevelNotesEditor
 
             if (currentFlow.ShrinkOverDistance)
                 mystyle.fontSize = fontSize - Mathf.Clamp((int)(size * dist), 0, fontSize - 1);
+            mystyle.normal.textColor = item.color;
 
-            if (item.showText)
+            Camera cam = SceneView.lastActiveSceneView.camera;
+            if (EditorApplication.isPlaying)
+                cam = Camera.current;
+
+            Plane[] frustum = GeometryUtility.CalculateFrustumPlanes(cam);
+            if (item.showText &&
+                GeometryUtility.TestPlanesAABB(frustum, new Bounds(item.position, Vector3.one)))
             {
                 Handles.Label(item.position, item.text, mystyle);
+            }
+
+            if (item.joinedPoint > -1)
+            {
+                Vector3 p1 = Vector3.zero, p2 = Vector3.zero;
+
+                Vector3 p0 = item.position;
+                Vector3 p3 = currentFlow.points[item.joinedPoint].position;
+                Vector3 dir = (p3 - p0).normalized;
+
+                float dotLeft = Vector3.Dot(dir, Vector3.left);
+                float dotUp = Vector3.Dot(dir, Vector3.up);
+
+                float absLeft = Mathf.Abs(dotLeft);
+                float absUp = Mathf.Abs(dotUp);
+
+                //if (absLeft > absUp)
+                //{
+                    p1 = p0 + (Vector3.left * 1.4f * (dotLeft < 0 ? -1 : 1));
+                    p2 = p3 - (Vector3.left * 1.4f * (dotLeft < 0 ? -1 : 1));
+                //}
+                //else
+                //{
+                //    p1 = p0 + (Vector3.up * (p3 - p0).magnitude * (dotUp < 0 ? -1 : 1));
+                //    p2 = p3 - (Vector3.up * (p3 - p0).magnitude * (dotUp < 0 ? -1 : 1));
+                //}
+
+                Handles.DrawBezier(p0, p3, p1, p2, new Color(0.7f, 0.7f, 0.7f), null, 2f);
             }
         }
     }
@@ -59,6 +95,7 @@ public class LevelNotesEditor
             if (Handles.Button(item.position, Quaternion.identity, size, size * 1.1f, Handles.DotHandleCap))
             {
                 currentPoint = item;
+                Selection.activeGameObject = null;
             }
         }
 
@@ -69,8 +106,20 @@ public class LevelNotesEditor
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(currentFlow, "Move Point");
-                EditorUtility.SetDirty(currentFlow);
                 currentPoint.position = point;
+            }
+
+            Event e = Event.current;
+            switch (e.type)
+            {
+                case EventType.KeyDown:
+                    {
+                        if (Event.current.keyCode == (KeyCode.Delete))
+                        {
+                            DeleteNote();
+                        }
+                        break;
+                    }
             }
         }
     }
@@ -108,6 +157,7 @@ public class LevelNotesEditor
         if (flow.Length == 0)
             return;
 
+        Undo.RecordObject(flow[0].gameObject, "Hide Notes");
         flow[0].gameObject.SetActive(false);
     }
 
@@ -118,6 +168,7 @@ public class LevelNotesEditor
         if (flow.Length == 0)
             return;
 
+        Undo.RecordObject(flow[0].gameObject, "Show Notes");
         flow[0].gameObject.SetActive(true);
     }
 
@@ -125,14 +176,22 @@ public class LevelNotesEditor
     static void HideText()
     {
         if (currentPoint != null)
+        {
+            Undo.RecordObject(currentFlow, "Hide Note");
+
             currentPoint.showText = false;
+        }
     }
 
     [MenuItem("Tools/Level Notes/Show Selected Note")]
     static void ShowText()
     {
         if (currentPoint != null)
+        {
+            Undo.RecordObject(currentFlow, "Show Note");
+
             currentPoint.showText = true;
+        }
     }
 
     [MenuItem("Tools/Level Notes/Edit Selected Note")]
@@ -149,6 +208,8 @@ public class LevelNotesEditor
     {
         if (editPoint == null)
             return;
+
+        Undo.RecordObject(currentFlow, "Edit Note");
 
         editPoint.text = text;
         editPoint = null;
@@ -169,10 +230,42 @@ public class LevelNotesEditor
     {
         if (confirmed)
         {
-            currentFlow.points.Remove(deletePoint);
+            Undo.RecordObject(currentFlow, "Delete Note");
+
+            currentFlow.RemoveItem(deletePoint);
         }
+
         deletePoint = null;
         currentPoint = null;
+    }
+
+    [MenuItem("Tools/Level Notes/Connect Note")]
+    static void JoinNote()
+    {
+        if (currentPoint == null)
+            return;
+
+        List<string> notes = new List<string>();
+        otherNotes = new List<LevelNotes.TextPoint>();
+        foreach (var item in currentFlow.points)
+        {
+            if (item == currentPoint)
+                continue;
+
+            notes.Add(item.text);
+            otherNotes.Add(item);
+        }
+
+        joinNode = currentPoint;
+        SelectorWindow.Show(JoinCallback, notes.ToArray());
+    }
+
+    static void JoinCallback(int index)
+    {
+        Undo.RecordObject(currentFlow, "Join Notes");
+
+        joinNode.joinedPoint = currentFlow.points.IndexOf(otherNotes[index]);
+        joinNode = null;
     }
 
 
@@ -259,5 +352,51 @@ public class ConfirmWindow : EditorWindow
             onFinish(false);
             Close();
         }
+    }
+}
+
+public class SelectorWindow : EditorWindow
+{
+    private UnityAction<int> onFinish;
+    private string[] texts;
+
+    private float itemHeight = 100;
+    private Vector2 scrollPos;
+
+    public static void Show(UnityAction<int> finishEvent, string[] texts)
+    {
+        var window = GetWindow<SelectorWindow>();
+        window.onFinish = finishEvent;
+        window.position = new Rect(window.position.x, window.position.y, 300, 400);
+        window.CenterOnMainWin();
+        window.texts = texts;
+
+        window.ShowPopup();
+    }
+
+    private void OnGUI()
+    {
+        float width = position.width - 25;
+        scrollPos = GUI.BeginScrollView(new Rect(5, 5, position.width - 10, position.height - 10), scrollPos,
+            new Rect(0, 0, width, (texts.Length + 1) * itemHeight), false, true);
+
+        if (GUI.Button(new Rect(0, 0, width, itemHeight), "None"))
+        {
+            onFinish(-1);
+            Close();
+            return;
+        }
+
+        for (int i = 0; i < texts.Length; ++i)
+        {
+            if (GUI.Button(new Rect(0, (i + 1) * itemHeight, width, itemHeight), texts[i]))
+            {
+                onFinish(i);
+                Close();
+                return;
+            }
+        }
+
+        GUI.EndScrollView();
     }
 }
--- Assets/Plugins/LevelNotes.cs
+++ Assets/Plugins/LevelNotes.cs
@@ -10,6 +10,8 @@ public class LevelNotes : MonoBehaviour
         public Vector3 position;
         [Multiline] public string text;
         public bool showText = true;
+        public int joinedPoint = -1;
+        public Color color = Color.white;
 
         public TextPoint(Vector3 position, string text)
         {
@@ -23,4 +25,19 @@ public class LevelNotes : MonoBehaviour
     public bool ShrinkOverDistance = true;
 
     public List<TextPoint> points = new List<TextPoint>();
+
+    public void RemoveItem(TextPoint item)
+    {
+        int index = points.IndexOf(item);
+
+        foreach (var point in points)
+        {
+            if (point.joinedPoint == index)
+                point.joinedPoint = -1;
+            else if (point.joinedPoint > index)
+                --point.joinedPoint;
+        }
+
+        points.Remove(item);
+    }
 }
