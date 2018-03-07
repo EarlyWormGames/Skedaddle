f186febbfe347941935d8730faccd34b9fe1974a
wickedarc <bernard.larkin@hotmail.com>
2018-03-07 11:32:59 +1100

    loris NV quick fix
    why this wasnt pushed last night, idk, I appoligise

--- Assets/Scripts/Animal/Loris.cs
+++ Assets/Scripts/Animal/Loris.cs
@@ -347,14 +347,14 @@ public class Loris : Animal
                 // LIGHT ON
                 for(int i = 0; i < m_lVisionLight.Length; ++i)
                     m_lVisionLight[i].intensity = Mathf.Lerp(m_lVisionLight[i].intensity, m_fOnIntensity, m_fLightSpeed * Time.deltaTime);
-                NV.NightVisionOn = false;
+                NV.NightVisionOn = true;
             }
             else
             {
                 // LIGHT OFF
                 for(int i = 0; i < m_lVisionLight.Length; ++i)
                     m_lVisionLight[i].intensity = Mathf.Lerp(m_lVisionLight[i].intensity, -1f, m_fLightSpeed * Time.deltaTime);
-                NV.NightVisionOn = true;
+                NV.NightVisionOn = false;
             }
         //}
 
