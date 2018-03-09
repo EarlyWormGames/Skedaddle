e39b43c1229d6dd2963de4045624e99eb0b2960f
Joshua Jones <joshuajones2010@gmail.com>
2018-03-09 16:45:26 +1100

    2-4 Cannon. Rat traps kill :rat:
    Reworked killer. Will have to go through previous levels to fix

--- Assets/Scripts/Object/Killer.cs
+++ Assets/Scripts/Object/Killer.cs
@@ -3,50 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class Killer : ActionObject
+public class Killer : MonoBehaviour
 {
     [System.Serializable]
     public class KillEvent : UnityEvent<Animal, DEATH_TYPE> { };
-
+    
     public DEATH_TYPE KillType;
-    public bool OnTrigger = true;
 
     public KillEvent OnKill;
 
-    protected override void OnCanTrigger()
+    void OnTriggerEnter(Collider other)
     {
-        if (!OnTrigger)
-        {
-            if (input.interact.wasJustPressed)
-            {
-                if (!Animal.CurrentAnimal.Alive)
-                    return;
+        var animal = other.attachedRigidbody.GetComponent<Animal>();
+        if (!animal.Alive)
+            return;
 
-                m_aCurrentAnimal = Animal.CurrentAnimal;
-                DoAction();
-            }
-        }
-    }
-
-    public override void AnimalEnter(Animal a_animal)
-    {
-        base.AnimalEnter(a_animal);
-        if (OnTrigger)
-        {
-            if (!a_animal.Alive)
-                return;
-
-            m_aCurrentAnimal = a_animal;
-            DoAction();
-        }
-    }
-
-    public override void DoAction()
-    {
-        base.DoAction();
-
-        m_aCurrentAnimal.Kill(KillType);
-        OnKill.Invoke(m_aCurrentAnimal, KillType);
-        m_aCurrentAnimal = null;
+        animal.Kill(KillType);
+        OnKill.Invoke(animal, KillType);
     }
 }
