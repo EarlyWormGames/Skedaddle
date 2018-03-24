a7dc7dce78cee20b5163e5f20f167fcd2dcd5d51
Joshua Jones <joshuajones2010@gmail.com>
2018-03-25 03:22:07 +1100

    Traps now triggered by crashmats

---
+++ Assets/Scripts/Helper/BasicTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BasicTrigger : MonoBehaviour
+{
+    [System.Serializable]
+    public class ColliderEvent : UnityEvent<Collider> { }
+
+    public bool AllowTriggers = false;
+    public ColliderEvent TriggerEnter, TriggerExit;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!AllowTriggers && other.isTrigger)
+            return;
+
+        TriggerEnter.Invoke(other);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!AllowTriggers && other.isTrigger)
+            return;
+
+        TriggerExit.Invoke(other);
+    }
+}
