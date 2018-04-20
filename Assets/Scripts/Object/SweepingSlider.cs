0be224e60a8753c7d64972d37bbb5ea99b90ec4d
Retr0mancer <mitchell.123@live.com.au>
2018-04-20 17:24:36 +1000

    more 2-9 fixes

--- Assets/Scripts/Object/SweepingSlider.cs
+++ Assets/Scripts/Object/SweepingSlider.cs
@@ -5,12 +5,27 @@ using UnityEngine;
 public class SweepingSlider : SlidingObject
 {
     public Rigidbody SweepBody;
+    public Transform[] GrowingObjects;
+    public float GrowMin = 1;
+    public float GrowMax = 2;
     public bool UseSweep { get; set; }
 
     protected override void OnStart()
     {
         base.OnStart();
         UseSweep = true;
+    }
+
+    protected override bool DoSlide(float time)
+    {
+        if(GrowingObjects.Length != 0)
+        {
+            foreach(Transform x in GrowingObjects)
+            {
+                x.localScale = new Vector3(x.localScale.x, Mathf.Lerp(movingForward ? GrowMin : GrowMax, movingForward ? GrowMax : GrowMin, MovingCurve.Evaluate( time / Speed)), x.localScale.z);
+            }
+        }
+        return base.DoSlide(time);
     }
 
     protected override bool DoMove(Vector3 position)
