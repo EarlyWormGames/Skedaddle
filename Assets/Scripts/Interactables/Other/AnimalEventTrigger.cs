5efe4c707d3489e175a4dd879bf7440637a002b1
Joshua Jones <joshuajones2010@gmail.com>
2018-05-15 15:26:59 +1000

    Animal trigger event script

---
+++ Assets/Scripts/Interactables/Other/AnimalEventTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class AnimalEventTrigger : AnimalTrigger
+{
+    public class AnimalEvent : UnityEvent<Animal> { }
+
+    public AnimalEvent OnAnimalEnter, OnAnimalExit;
+
+    public override void AnimalEnter(Animal animal)
+    {
+        OnAnimalEnter.Invoke(animal);
+    }
+
+    public override void AnimalExit(Animal animal)
+    {
+        OnAnimalExit.Invoke(animal);
+    }
+}
